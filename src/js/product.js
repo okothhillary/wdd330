662737b0e9366e545019ca79a1f751d6ec44b2ed
okothhillary <odu22002@byui.edu>
2025-01-20 23:13:10 +0300

    netlify fix

--- src/js/product.js
+++ src/js/product.js
@@ -4,4 +4,4 @@ import productDetails from "./ProductDetails.mjs";
 const productId = getParam("product");
 productDetails(productId);
 
-//should be working
+//should be working now and reflecting on netlify
