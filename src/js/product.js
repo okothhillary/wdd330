7d10bf2ddc448cb24356af02bb80531c5fd84951
okothhillary <odu22002@byui.edu>
2025-01-20 13:19:31 +0300

    fixing add to cart button

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,7 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  //passing cart to ensure it's an array
+  //passing cart to ensure it's an array; fix for add to cart button not working.
   let cart = JSON.parse(localStorage.getItem("so-cart"));
   if (!Array.isArray(cart)) {
     cart = []; // creating empty cart
