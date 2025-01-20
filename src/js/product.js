47ca15f8b05668bfdada02171db20f0a8b65a022
okothhillary <odu22002@byui.edu>
2025-01-20 15:53:11 +0300

    k

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,5 @@
 import { getParam } from "./utils.mjs";
-import productDetails from "./productDetails.mjs";
+import productDetails from "./ProductDetails.mjs";
 
 const productId = getParam("product");
 productDetails(productId);
