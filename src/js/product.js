b357989041c3b378941da87ebb4674e485b0bbaa
okothhillary <odu22002@byui.edu>
2025-01-21 16:29:04 +0300

    still crying

--- src/js/product.js
+++ src/js/product.js
@@ -1,17 +1,5 @@
-export default defineConfig({
-    root: "src/",
-    build: {
-      outDir: "../dist",
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, "src/index.html"),
-          cart: resolve(__dirname, "src/cart/index.html"),
-          product: resolve(__dirname, "src/product_pages/index.html"),
-        },
-      },
-    },
-    optimizeDeps: {
-      include: ['utils.mjs', 'ProductDetails.mjs'], // Ensure Vite handles these dependencies
-    },
-  });
-  
+import { getParam } from "./utils.mjs";
+import productDetails from "./ProductDetails.mjs";
+
+const productId = getParam("product");
+productDetails(productId);
