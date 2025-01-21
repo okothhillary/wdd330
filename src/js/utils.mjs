b81f6bdb73fffd5d0ec0d5278fefa3c2c7eaa32c
okothhillary <odu22002@byui.edu>
2025-01-21 13:38:02 +0300

    correct parameter function

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -24,8 +24,6 @@ export function setClick(selector, callback) {
 
 export function getParam(param) {
   const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get('product');
-
-  return product;
+  const urlParams = new URLSearchParams(queryString); 
+  return urlParams.get(param); 
 }
